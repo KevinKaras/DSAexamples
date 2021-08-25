326c321032132053e47bad2b25fc4a776548dfd4
KevinKaras <kevinkaras2@gmail.com>
2021-08-24 23:37:34 -0500

    added resources to read when no longer preparing to move to florida

--- DEPRECATED-data-structures-master/TestCodingChallenges/test1.js
+++ DEPRECATED-data-structures-master/TestCodingChallenges/test1.js
@@ -1,1 +1,5 @@
 // run through binary search tree videos before beginning. 
+
+//  some resources that may be helpful to refresh on some basics
+// https://www.youtube.com/watch?v=t2CEgPsws3U
+// https://www.youtube.com/watch?v=8K7EO7s_iFE
